a18265fbb9db2786f226cb6faacd01fe51e72712
lukeclaark <luke.clark5129@gmail.com>
2025-07-09 09:31:11 -0500

    Update streamlit_app.py
    panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -10,7 +11,7 @@ st.write(
   """
 )
 
-import streamlit as st
+
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
